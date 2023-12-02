578ac39eeacd7e2a5129fbd62a59cb140ccf85d0
ViNu-23 <vijayvinu46@gmail.com>
2023-12-02 21:43:16 +0530

    complete app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+//assigning url link to url constant
+const url = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  //assigning a null array  to user
+  const [user, setUser] = useState([]);  
+
+  //fetching the data using Async await
+  async function getData() {
+  const response = await fetch(url);
+  const userData = await response.json();
+  setUser(userData);
+  console.log(userData);
+};
+  //rendering getData() function only one time while page reloading
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //returning components
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* using map function loop through all values that are stored in user that assigned from url json */}
+      {user.map((i)=>{
+        return (
+     <div className="user-data">
+        <h2 key={i.id}>Name: {i.name}</h2>
+        <p> User Name: {i.username}</p>
+        <p>Email: {i.email}</p>
+        <p>Website: {i.website}</p>
+
+     </div>
+        )
+      })}
+    </>
   );
 }
-
 export default App;
